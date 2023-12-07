286d2da0ff3111f2cc7d954132aadbe5b6ee65cf
RodrigueBoulay <153192812+RodrigueBoulay@users.noreply.github.com>
2023-12-07 13:54:22 +0100

    Add files via upload

---
+++ Zanzibar/fonction.h
@@ -0,0 +1,11 @@
+#ifndef MOLKKY_FONCTION_H
+#define MOLKKY_FONCTION_H
+
+#include <iostream>
+
+using namespace std;
+
+double des();
+
+
+#endif //MOLKKY_FONCTION_H
---
+++ Zanzibar/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "fonction.h"
+
+int main() {
+    des();
+    return 0;
+}
